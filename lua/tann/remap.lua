4ca5f5a6292b0220daec623f388cf157db353f00
tann <gadkaritanishq@gmail.com>
2023-10-10 10:17:42 -0700

    keymap for toggling relative numbers

--- lua/tann/remap.lua
+++ lua/tann/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex) -- e -> explore
+vim.keymap.set("n", "<leader>rn", function () -- r -> relative; n -> number
+   vim.wo.relativenumber = not vim.wo.relativenumber
+end)
