2250dbdeb04bb3e83ae3db1c7de5ae2012b7c0ad
tann <gadkaritanishq@gmail.com>
2023-10-24 12:15:33 -0700

    added a toggle functionality with keymapping for diagnostic

--- init.lua
+++ init.lua
@@ -6,4 +6,16 @@ vim.o.hlsearch = false
 vim.o.mouse = 'a'
 
 vim.cmd("colorscheme kanagawa-wave")
-vim.diagnostic.config({signs = false})
+vim.api.nvim_create_user_command(
+  'DiagnosticsToggleVirtualText',
+  function()
+    local current_value = vim.diagnostic.config().virtual_text
+    if current_value then
+      vim.diagnostic.config({virtual_text = false})
+    else
+      vim.diagnostic.config({virtual_text = true})
+    end
+  end,
+  {}
+)
+vim.keymap.set("n", "<leader>td", vim.cmd.DiagnosticsToggleVirtualText) -- t -> toggle; d -> diagnostics
